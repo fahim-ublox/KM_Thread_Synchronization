2eb1ee60ba3ee0bf0de346ceadf6ae90b1808f4a
Mudassar Hussain <mudassar.hussain@u-blox.com>
2017-11-08 14:57:20 +0500

    Thread, Mutex and Semaphore application program

--- main.cpp
+++ main.cpp
@@ -1,39 +1,77 @@
-
 #include "mbed.h"
 #include "rtos.h"
 
+
+DigitalOut led1(LED1);
+
+Semaphore sem(1);
 Mutex mutex;
 
-void Th_print_1(const char* name, int state) {
-	mutex.lock();
-    printf("Thread 1\n\r");
-    mutex.unlock();
+int gA=1;
+int gB;
+const int C = 10;
+
+static void myHeap_Address()
+{
+	//pc.printf("address gA: %d\n", &gA);
+ 	//pc.printf("address gB: %d\n", &gB);
+ 	//pc.printf("address constant C: %d\n", &C);
 }
 
-void Th_print_2(const char* name, int state) {
+/* Semaphore is added in this function but released in 2nd thread function to check semaphore working or not.
+ * "Th_print_1' Function execution will be halted until 2nd thread function releases semaphore.
+ */
+void Th_print_1(void) {
+	int32_t value=0;
+
+	value = sem.wait();
+    printf("\nSemaphore wait: %d\n\r",value);
+
+    printf("\nSemaphore Working\n\r");
+    //sem.release();
+}
+
+/* Mutex is locked in this function and No other thread can access this function until mutex is unlocked.
+ * and Semaphore is also released here.
+ */
+void Th_print_2(void) {
+
 	mutex.lock();
-    printf("Thread 1\n\r");
+
+    printf("\nMutex Working\n\r");
+
+    sem.release();
+
     mutex.unlock();
 }
 
 void test_thread_1(void const *args) {
-    while (true) {
-    	Th_print_1((const char*)args, 0);
-        Thread::wait(1000);
-    }
+
+	while(true){
+		Th_print_1();
+		Thread::wait(30);
+	}
 }
 
 void test_thread_2(void const *args) {
+
+	while(true){
+		Th_print_2();
+		Thread::wait(100);
+	}
+}
+
+
+int main (void) {
+    Thread t1(osPriorityHigh);
+    Thread t2(osPriorityAboveNormal);
+
+    t1.start(callback(test_thread_1, (void *)"Semaphore Thread"));
+    t2.start(callback(test_thread_2, (void *)"Mutex Thread"));
+
+
     while (true) {
-    	Th_print_2((const char*)args, 0);
-        Thread::wait(1000);
+    	myHeap_Address();
     }
 }
 
-int main() {
-    Thread t1(test_thread_1);
-    Thread t2(test_thread_2);
-
-    test_thread_1((void *)"Th 1");
-    test_thread_2((void *)"Th 2");
-}
